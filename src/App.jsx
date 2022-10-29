fa249dbf07510a61549ebcc8fed6e0c8b6067a5c
Fernando Cavalcanti <comododk@gmail.com>
2022-10-29 18:55:20 -0300

    feat: created style global

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import Post from './Post.jsx';
+import { Header } from './components/Header';
+
+import './globalStyles.css';
 
 export function App() {
 
   return (
     <>
-      <Post
-        author="Fernando Cavalcanti"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam quaerat quidem adipisci amet, assumenda ex, voluptatibus debitis dolor veniam blanditiis. Facere, quaerat debitis dicta tempora odio ipsa qui officiis."
-      />
-
-      <Post
-        author="Cavalcanti"
-        content="blanditiis. Facere, quaerat debitis dicta tempora odio ipsa qui officiis."
-      />
+      <Header />
+      <h1>teste aaa</h1>
     </>
   )
 }
