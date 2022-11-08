db32c2f11e26f0421e09c10e08cce4478dcd3469
Fernando Cavalcanti <comododk@gmail.com>
2022-11-07 23:33:02 -0300

    feat: form validetor

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -24,7 +24,12 @@ export function Post({ data }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório');
   }
 
   function deleteComment(commentToDelete){
@@ -35,6 +40,7 @@ export function Post({ data }) {
     setComments(commentsWithoutDeletedOne);
   }
 
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -74,10 +80,12 @@ export function Post({ data }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
         </footer>
       </form>
 
